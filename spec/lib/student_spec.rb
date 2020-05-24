efc4c9a7e445cda56d9c7efcea580b2f6b6d24f7
cwperry <christopher.perry@employbridge.com>
2020-05-23 20:04:59 -0500

    Created constructor test for Student

--- spec/lib/student_spec.rb
+++ spec/lib/student_spec.rb
@@ -1,5 +1,13 @@
 RSpec.describe Student do
-  it "should fail" do
-    expect(true).to eq(false)
+  describe 'constructor' do
+    it 'should create a well formed student' do
+      student = Student.new('Chester', 'Nimitz', 135, 99, 95 , 97)
+      expect(student.first_name).to eq('Chester')
+      expect(student.last_name).to eq('Nimitz')
+      expect(student.age).to eq(135)
+      expect(student.math_grade).to eq(99)
+      expect(student.physics_grade).to eq(95)
+      expect(student.average).to eq(97)
+    end
   end
 end
