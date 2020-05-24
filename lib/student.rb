632d69f1ab0ca2de46a7824937f3277ba9459474
cwperry <christopher.perry@employbridge.com>
2020-05-23 20:09:41 -0500

    Added constructor and accessors to Student

--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,18 @@
 class Student
+
+  attr_reader :age,
+              :average,
+              :physics_grade,
+              :math_grade,
+              :first_name,
+              :last_name
+
+  def initialize(first_name, last_name, age, math_grade, physics_grade, average)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+    @math_grade = math_grade
+    @physics_grade = physics_grade
+    @average = average
+  end
 end
