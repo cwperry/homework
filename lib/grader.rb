aeba446147d1be00b9bb381c5e4af52e6964873f
cwperry <christopher.perry@employbridge.com>
2020-05-24 10:27:02 -0500

    Implemented Grader constructor
    The constructor takes the provided file, converts it to a csv dataset, iterates through it, and creates an array of Students.

--- lib/grader.rb
+++ lib/grader.rb
@@ -1,7 +1,22 @@
-require "csv"
-require "student"
+require 'csv'
+require 'student'
 
 class Grader
+
+  attr_reader :students
+
   def initialize(file)
+    @students = []
+    table = CSV.parse(File.read(file), headers: true)
+    table.each { |row| @students.push(create_student(row)) }
   end
+end
+
+def create_student(row)
+  Student.new(row['first_name'],
+              row['last_name'],
+              row['age'],
+              row['english_grade'],
+              row['math_grade'],
+              row['physics_grade'])
 end
