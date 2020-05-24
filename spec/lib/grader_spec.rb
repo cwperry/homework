267e44647c1e9ccc3ea0bab61b881ad53225b122
cwperry <christopher.perry@employbridge.com>
2020-05-24 10:03:38 -0500

    Created test to load csv and create students in Grader

--- spec/lib/grader_spec.rb
+++ spec/lib/grader_spec.rb
@@ -1,5 +1,13 @@
 RSpec.describe Grader do
-  it "should pass" do
-    expect(true).to eq(true)
+  describe 'constructor' do
+    it 'should convert the provided file into a csv and create student instances from the data' do
+      file = File.open('./spec/files/test_grades.csv')
+      grader = Grader.new(file)
+
+      expect(grader.students.size).to eq(3)
+      expect(grader.students[0].name).to eq('Chester Nimitz')
+      expect(grader.students[1].name).to eq('Raymond Spruance')
+      expect(grader.students[2].name).to eq('Charles Lockwood')
+    end
   end
 end
