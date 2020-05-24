7d4bfdeef53c5948a84ff5ecc590598b5873f7a6
cwperry <christopher.perry@employbridge.com>
2020-05-24 11:05:53 -0500

    Added logic to scrape.rb to print output
    I didn't know how to test the Terminal, so I didn't.
    
    This was a fun exercise, and I look forward to talking with you.
    
    Thank you for the opportunity.
    
    Chris

--- scrape.rb
+++ scrape.rb
@@ -1,11 +1,25 @@
-require "grader"
-require "terminal-table"
+require './lib/grader'
+require 'terminal-table'
 
-file = File.open("./files/grades.csv")
+file = File.open('./files/grades.csv')
 grader = Grader.new(file)
 
 table = Terminal::Table.new do |t|
-  t << ["Sample", "Output", "Here"]
+  t << %w[Name English Math Physics Average]
+  t << :separator
+
+  grader.students.each do |student|
+    t << [student.name,
+          student.english_grade,
+          student.math_grade,
+          student.physics_grade,
+          student.average]
+
+    if grader.students.index(student) != grader.students.size - 1
+      t << :separator
+    end
+
+  end
 end
 
 puts table
