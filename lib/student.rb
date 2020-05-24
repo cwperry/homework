46d9eac5af2420b72cdd6227fb7f4833e83e7828
cwperry <christopher.perry@employbridge.com>
2020-05-23 20:27:09 -0500

    Added english_grade instead of average to constructor
    I mistook the the last column in the csv dataset to be the average. I missed the english_grade column.
    
    I reordered the attr_reader parameters to be in the same order as the constructor parameters.

--- lib/student.rb
+++ lib/student.rb
@@ -1,22 +1,23 @@
 class Student
 
-  attr_reader :age,
-              :average,
-              :physics_grade,
+  attr_reader :first_name,
+              :last_name,
+              :age,
+              :english_grade,
               :math_grade,
-              :first_name,
-              :last_name
+              :physics_grade
 
-  def initialize(first_name, last_name, age, math_grade, physics_grade, average)
+  def initialize(first_name, last_name, age, english_grade, math_grade, physics_grade)
     @first_name = first_name
     @last_name = last_name
     @age = age
+    @english_grade = english_grade
     @math_grade = math_grade
     @physics_grade = physics_grade
-    @average = average
   end
 
   def name
     @first_name + ' ' + @last_name
   end
+
 end
--- spec/lib/student_spec.rb
+++ spec/lib/student_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe Student do
-  student = Student.new('Chester', 'Nimitz', 135, 99, 95 , 97)
+  student = Student.new('Chester', 'Nimitz', 135, 99, 95, 97)
 
   describe 'constructor' do
     it 'should create a well formed student' do
       expect(student.first_name).to eq('Chester')
       expect(student.last_name).to eq('Nimitz')
       expect(student.age).to eq(135)
-      expect(student.math_grade).to eq(99)
-      expect(student.physics_grade).to eq(95)
-      expect(student.average).to eq(97)
+      expect(student.english_grade).to eq(99)
+      expect(student.math_grade).to eq(95)
+      expect(student.physics_grade).to eq(97)
     end
   end
 
